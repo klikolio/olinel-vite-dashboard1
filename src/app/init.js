1a303e78b8e925a5e25ab07dd0cdfc982fcc967a
klikolio <klikolio.creative@gmail.com>
2024-01-29 14:57:34 +0700

    Feather icon integration

--- src/app/init.js
+++ src/app/init.js
@@ -1,3 +1,4 @@
 import '@/app/utilities/fontawesome-components'
+import '@/app/utilities/feather-components'
 import '@/app/utilities/simplebar-components'
 import '@/app/utilities/bootstrap-components'
