ad272fa989cbf6ba876c43cd0292c9635012dc64
klikolio <klikolio.creative@gmail.com>
2024-01-30 20:11:15 +0700

    Add RTL and legacy browser supports

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import postcssRTLCSS from 'postcss-rtlcss'
+
+export default {
+  plugins: [
+    process.env['PAGE_DIRECTION'] === 'rtl'
+      ? postcssRTLCSS({ mode: 'override' })
+      : undefined
+  ],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
-import { defineConfig } from 'vite'
+import 'dotenv/config'
 import vituum from 'vituum'
 import nunjucks from '@vituum/vite-plugin-nunjucks'
+import legacy from '@vitejs/plugin-legacy'
+import { defineConfig } from 'vite'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
+  publicDir: 'public',
+  build: {
+    minify: 'terser',
+    cssMinify: 'lightningcss'
+  },
   resolve: {
     alias: {
       '@': '/src'
     }
   },
-  plugins: [vituum(), nunjucks({
+  plugins: [vituum(), legacy(), ViteMinifyPlugin(), nunjucks({
     root: './src',
+    globals: {
+      env: {
+        PAGE_DIRECTION: process.env['PAGE_DIRECTION'] ?? 'ltr'
+      }
+    },
     filters: {
       array_concat: (arr1, arr2) => (typeof arr1 === 'object' && typeof arr2 === 'object' ? [...arr1, ...arr2] : arr1),
       classnames: (classNamesObj) => {
