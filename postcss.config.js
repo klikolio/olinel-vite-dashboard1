10ac6ccaa4e2afccbca7df10b7d103bf93ca2e00
klikolio <klikolio.creative@gmail.com>
2024-01-30 21:13:47 +0700

    Add lint

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,5 @@ import 'dotenv/config'
 import postcssRTLCSS from 'postcss-rtlcss'
 
 export default {
-  plugins: [
-    process.env.PAGE_DIRECTION === 'rtl'
-      ? postcssRTLCSS({ mode: 'override' })
-      : undefined
-  ]
+	plugins: [process.env.PAGE_DIRECTION === 'rtl' ? postcssRTLCSS({ mode: 'override' }) : undefined],
 }
--- vite.config.js
+++ vite.config.js
@@ -6,34 +6,37 @@ import { defineConfig } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
-  publicDir: 'public',
-  build: {
-    minify: 'terser',
-    cssMinify: 'lightningcss'
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  plugins: [
-    vituum(),
-    ViteMinifyPlugin(),
-    legacy(),
-    nunjucks({
-      root: './src',
-      globals: {
-        env: {
-          PAGE_DIRECTION: process.env.PAGE_DIRECTION ?? 'ltr'
-        }
-      },
-      filters: {
-        classnames: (classNamesObj) => {
-          return Object.keys(classNamesObj).map((className) => {
-            return classNamesObj[className] ? className : null
-          }).filter((className) => className !== null).join(' ')
-        }
-      }
-    })
-  ]
+	publicDir: 'public',
+	build: {
+		minify: 'terser',
+		cssMinify: 'lightningcss',
+	},
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
+	plugins: [
+		vituum(),
+		ViteMinifyPlugin(),
+		legacy(),
+		nunjucks({
+			root: './src',
+			globals: {
+				env: {
+					PAGE_DIRECTION: process.env.PAGE_DIRECTION ?? 'ltr',
+				},
+			},
+			filters: {
+				classnames: (classNamesObj) => {
+					return Object.keys(classNamesObj)
+						.map((className) => {
+							return classNamesObj[className] ? className : null
+						})
+						.filter((className) => className !== null)
+						.join(' ')
+				},
+			},
+		}),
+	],
 })
